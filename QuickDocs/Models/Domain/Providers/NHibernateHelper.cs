d82e28fb52b771bc8920f49c5b57a1929dc1f0c2
WonderfulFalcon <osakovich@gmail.com>
2014-12-26 16:03:59 +0300

    DQD-2 Добавление Хелпера, файла конфигурации, базового провайдера данных

---
+++ QuickDocs/Models/Domain/Providers/NHibernateHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace QuickDocs.Models.Domain.Providers
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    configuration.AddAssembly(Assembly.GetCallingAssembly());
+                    _sessionFactory = configuration.BuildSessionFactory();
+                }
+
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+    }
+}
