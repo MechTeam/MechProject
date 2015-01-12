37c7748dbdcbb65f50d6b0dc7988e57cf55d16de
Daniil-Mel <Danya-Mel@mail.ru>
2015-01-12 16:53:08 +0300

    Реализовать класс Membership и добавить соответствующий файл маппинга

---
+++ QuickDocs/Models/Domain/Entities/Membership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class Membership
+    {
+        public virtual Guid ID { get; protected set; }
+        public virtual DateTime CreateDate { get; protected set; }
+        public virtual int ConfirmationToken { get; set; }
+        public virtual bool IsConfirmed { get; set; }
+        public virtual DateTime LastPasswordFailureDate { get; set; }
+        public virtual int PasswordFailuresSinceLastSuccess { get; set; }
+        public virtual string Password { get; set; }
+        public virtual DateTime PasswordChangeDate { get; set; }
+        public virtual string PasswordSalt { get; set; }
+        public virtual int PasswordVerificationToken { get; set; }
+        public virtual bool PasswordVerificationTokenExpiration { get; set; }
+    }
+}
