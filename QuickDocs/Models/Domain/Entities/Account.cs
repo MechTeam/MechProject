fdb0dd7b4c02746d4c0ca201571bd36f6c8d00fc
WonderfulFalcon <osakovich@gmail.com>
2015-01-15 14:34:50 +0300

    Bug fix

---
+++ QuickDocs/Models/Domain/Entities/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class Account
+    {
+        public virtual Guid ID { get; protected set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual int ConfirmationToken { get; set; }
+        public virtual bool IsConfirmed { get; set; }
+        public virtual string Password { get; set; }
+        public virtual User User { get; set; }
+    }
+}
