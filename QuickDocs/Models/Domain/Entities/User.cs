a575a23b29eabe11328bcc8cf2a64481203020a7
WonderfulFalcon <osakovich@gmail.com>
2015-01-21 16:17:37 +0300

    Правка объектной модели: файлы User.hbm.xml, Account.hbm.xml, User.cs

--- QuickDocs/Models/Domain/Entities/User.cs
+++ QuickDocs/Models/Domain/Entities/User.cs
@@ -8,8 +8,8 @@ namespace QuickDocs.Models.Domain.Entities
     public class User
     {
         public virtual Guid ID { get; protected set; }
-        public virtual string Name { get; set; }
         public virtual string SecondName { get; set; }
+        public virtual string FirstName { get; set; }
         public virtual string Email { get; set; }
     }
 }
