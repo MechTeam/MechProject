1dff886f06dfacbe47f936b877dff1c044e2a7ad
WonderfulFalcon <osakovich@gmail.com>
2015-01-21 16:34:46 +0300

    Создание провайдера AccountProvider

---
+++ QuickDocs/Models/Domain/Filters/AuthenticationFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Filters
+{
+    public class AuthenticationFilter
+    {
+        public Guid IDUser { get; set; }
+        public string EMail { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ QuickDocs/Models/Domain/Providers/AccountProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using QuickDocs.Models.Domain.Entities;
+using QuickDocs.Models.Domain.Filters;
+
+namespace QuickDocs.Models.Domain.Providers
+{
+    public class AccountProvider : BaseProvider<Account>
+    {
+        public IList<AuthenticationFilter> GetListAuthentication()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    List<AuthenticationFilter> listAuth = new List<AuthenticationFilter>();
+                    foreach (var item in session.CreateCriteria(typeof(Account))
+                        .List<Account>())
+                    {
+                        listAuth.Add(new AuthenticationFilter() { IDUser = item.ID, Password = item.Password, EMail = item.User.Email });
+                    }
+                    return listAuth;
+                }
+            }
+        }
+    }
+}
