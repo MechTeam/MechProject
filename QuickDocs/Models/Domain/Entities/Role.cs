4ac70788e1d31b6465cc4104e6cff6401942f968
AndreyCruchok <cruchok.andrey@yandex.ru>
2015-01-26 01:06:03 +0300

    SQD-2 добавить класс Role и соответствующий файл маппинга.

---
+++ QuickDocs/Models/Domain/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class Role
+    {
+        public virtual Guid ID { get; protected set; }
+        public virtual string Name { get; set; }
+    }
+}
