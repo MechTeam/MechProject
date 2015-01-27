93b2da01efff9116ed86301be0ca2828c5979529
shurygin1996 <shurygin_1@mail.ru>
2015-01-27 21:43:58 +0300

    Реализовать класс Control и добавить соответствующий файл маппинга

---
+++ QuickDocs/Models/Domain/Entities/Control.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class Control
+    {
+        public virtual Guid ID {get; protected set;}
+        public virtual string Name { get; set; }
+        public virtual int MaxPoints { get; set; }
+        public virtual int MinPoints { get; set; }
+        public virtual Guid ID_ControlType { get; set; }
+        public virtual Guid ID_Discipline { get; set; }
+        public virtual int ModuleNum { get; set; }
+        public virtual int NumControl { get; set; }
+        public virtual int Week { get; set; }
+    }
+}
