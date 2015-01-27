f4a2e3568fe851f6abbe6e887698d0c2a05284ce
AndreyCruchok <cruchok.andrey@yandex.ru>
2015-01-28 00:13:38 +0300

    SQD-11 Реализовать класс StudentInLesson и добавить соответствующий файл маппинга.

---
+++ QuickDocs/Models/Domain/Entities/StudentInLesson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class StudentInLesson
+    {
+        public virtual Schedule Schedule { get; set; }
+        public virtual Guid ID { get; set; }
+        public virtual Discipline Discipline { get; set; }
+        public virtual StudentGroup StudentGroup { get; set; }
+        public virtual Teacher Teacher { get; set; }
+        public virtual LessonType LessonType { get; set; }
+        public virtual LessonNum LessonNum { get; set; }
+        public virtual int LectureRoom { get; set; }
+    }
+}
