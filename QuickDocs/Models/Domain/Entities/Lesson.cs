15d88f03ac4b003f553d1654922b38e6f28094b8
WonderfulFalcon <osakovich@gmail.com>
2015-01-28 17:42:19 +0300

    Реализовать класс Schedule и добавить соответствующий файл маппинга: Исправлено: Класс переименован в Lesson.cs, добавлены свойства навигации к соотвствующим сущностям

---
+++ QuickDocs/Models/Domain/Entities/Lesson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class Lesson
+    {
+        public virtual Guid ID { get; protected set; }
+        public virtual Discipline Discipline { get;  set; }
+        public virtual StudentGroup StudentGroup { get;  set; }
+        public virtual Teacher Teacher { get;  set; }
+        public virtual LessonType LessonType { get;  set; }
+        public virtual LessonNum LessonNum { get;  set; }
+        public virtual string LectureRoom { get; set; }
+    }
+}
