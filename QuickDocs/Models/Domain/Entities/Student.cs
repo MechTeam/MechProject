e1787b25f900adb7b691637d509ec87562dafe59
WonderfulFalcon <osakovich@gmail.com>
2015-01-28 18:50:55 +0300

    Реализовать класс Student и добавить соответствующий файл маппинга

---
+++ QuickDocs/Models/Domain/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class Student
+    {
+        public virtual User User { get; set; } 
+        public virtual Guid Id { get; set; }
+        public virtual StudentGroup StudentGroup { get; set; } 
+    }
+}
