fd313a75d15d05bded23c5765234ba6a607e664e
WonderfulFalcon <osakovich@gmail.com>
2015-01-28 22:33:39 +0300

    SQD-2 добавить класс Role и соответствующий файл маппинга. Дополнительно изменил класс Юзер,

--- QuickDocs/Models/Domain/Entities/User.cs
+++ QuickDocs/Models/Domain/Entities/User.cs
@@ -11,5 +11,10 @@ namespace QuickDocs.Models.Domain.Entities
         public virtual string SecondName { get; set; }
         public virtual string FirstName { get; set; }
         public virtual string Email { get; set; }
+        public virtual IList<Role> Roles { get; set; }
+        public User()
+        {
+            Roles = new List<Role>();
+        }
     }
 }
