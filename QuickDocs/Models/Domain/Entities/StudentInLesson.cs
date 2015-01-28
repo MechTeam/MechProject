abc33f735e087d2c80b7ea52884c45d5d0416346
WonderfulFalcon <osakovich@gmail.com>
2015-01-28 17:56:52 +0300

    Реализовать класс StudentInLesson и добавить соответствующий файл маппинга. Исправлено: добавлены свойства навигации на Student и Lesson

--- QuickDocs/Models/Domain/Entities/StudentInLesson.cs
+++ QuickDocs/Models/Domain/Entities/StudentInLesson.cs
@@ -7,13 +7,8 @@ namespace QuickDocs.Models.Domain.Entities
 {
     public class StudentInLesson
     {
-        public virtual Schedule Schedule { get; set; }
         public virtual Guid ID { get; set; }
-        public virtual Discipline Discipline { get; set; }
-        public virtual StudentGroup StudentGroup { get; set; }
-        public virtual Teacher Teacher { get; set; }
-        public virtual LessonType LessonType { get; set; }
-        public virtual LessonNum LessonNum { get; set; }
-        public virtual int LectureRoom { get; set; }
+        public virtual Lesson Lesson { get; set; }
+        public virtual Student Student { get; set; }
     }
 }
