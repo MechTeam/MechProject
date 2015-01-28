1afbaebcdd0ebd50ce8ce58805b3ab1630d8c801
WonderfulFalcon <osakovich@gmail.com>
2015-01-28 17:20:48 +0300

    Реализовать класс Teacher и добавить соответствующий файл маппинга: Исправлено: добавлена ссылка на класс User

--- QuickDocs/Models/Domain/Entities/Teacher.cs
+++ QuickDocs/Models/Domain/Entities/Teacher.cs
@@ -7,7 +7,8 @@ namespace QuickDocs.Models.Domain.Entities
 {
     public class Teacher
     {
-        public virtual Guid ID_User { get; protected set; }
+        public virtual Guid ID { get; protected set; }
+        public virtual User User { get; set; }
         public virtual string Part { get; set; }
         public virtual string Post { get; set; }
     }
