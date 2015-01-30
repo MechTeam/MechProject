15f1adb7782e858a7b2a4be33ad7f251f1e93e9c
AndreyCruchok <cruchok.andrey@yandex.ru>
2015-01-31 00:31:58 +0300

    SQD-12 Реализовать класс ProgressOfStudent и добавить соответствующий файл маппинга.

---
+++ QuickDocs/Models/Domain/Entities/ProgressOfStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Entities
+{
+    public class ProgressOfStudent
+    {
+        public virtual StudentInLesson ID_StudentInLesson { get; set; }
+        public virtual Control ID_Control { get; set; }
+        public virtual int Points { get; set; }
+        public virtual DateTime Date { get; set; }
+    }
+}
