b8f0a5739e5b87de7fbfc538827f7ccfb3174f57
WonderfulFalcon <osakovich@gmail.com>
2015-01-31 15:57:36 +0300

    Реализовать класс ProgressOfStudent и добавить соответствующий файл маппинга. Исправлено: имена свойств

--- QuickDocs/Models/Domain/Entities/ProgressOfStudent.cs
+++ QuickDocs/Models/Domain/Entities/ProgressOfStudent.cs
@@ -7,8 +7,8 @@ namespace QuickDocs.Models.Domain.Entities
 {
     public class ProgressOfStudent
     {
-        public virtual StudentInLesson ID_StudentInLesson { get; set; }
-        public virtual Control ID_Control { get; set; }
+        public virtual StudentInLesson StudentInLesson { get; set; }
+        public virtual Control Control { get; set; }
         public virtual int Points { get; set; }
         public virtual DateTime Date { get; set; }
     }
