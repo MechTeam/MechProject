3c206bff28c1ccd422248adbe59b8c77e77eaf04
Daniil-Mel <Danya-Mel@mail.ru>
2015-02-04 14:51:28 +0300

    Добавить в проект, в папку \Models\Domain\Providers, провайдеров данных, унаследованных от базового провайдера, для следующих сущностей -Student -Discipline -Lesson -LessonType -StudentInLesson

---
+++ QuickDocs/Models/Domain/Providers/StudentInLessonProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuickDocs.Models.Domain.Entities;
+
+namespace QuickDocs.Models.Domain.Providers
+{
+    public class StudentInLessonProvider:BaseProvider<StudentInLesson>
+    {
+    }
+}
