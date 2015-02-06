bafda7383847d6a281b97effe8f51a535f297968
Daniil-Mel <Danya-Mel@mail.ru>
2015-02-06 16:34:35 +0300

    Правка провайдеров данных Teacher и Student

--- QuickDocs/Models/Domain/Providers/StudentProvider.cs
+++ QuickDocs/Models/Domain/Providers/StudentProvider.cs
@@ -8,5 +8,29 @@ namespace QuickDocs.Models.Domain.Providers
 {
     public class StudentProvider : BaseProvider<Student>
     {
+        public new void Add(Student student)
+        {
+            UserProvider userProvider = new UserProvider();
+            StudentProvider studentProvider = new StudentProvider();
+            RoleProvider roleProvider = new RoleProvider();
+
+            if (student.User == null)
+            {
+                throw new Exception(String.Format("Поля не заполнены!"));
+            }
+
+            if (userProvider.GetByID(student.ID) == null)
+            {
+                userProvider.Add(student.User);
+            }
+
+            if (GetByID(student.ID) == null)
+            {
+                studentProvider.Add(student);
+
+                 Role role = roleProvider.GetByName("Student");
+                 userProvider.AddUserToRole(student.User, role);
+            }
+        }
     }
 }
--- QuickDocs/Models/Domain/Providers/TeacherProvider.cs
+++ QuickDocs/Models/Domain/Providers/TeacherProvider.cs
@@ -6,7 +6,31 @@ using QuickDocs.Models.Domain.Entities;
 
 namespace QuickDocs.Models.Domain.Providers
 {
-    public class TeacherProvider : BaseProvider<Teacher>
+    public class TeacherProvider:BaseProvider<Teacher>
     {
+        public new void Add(Teacher teacher)
+        {
+            UserProvider userProvider = new UserProvider();
+            TeacherProvider teacherProvider = new TeacherProvider();
+            RoleProvider roleProvider = new RoleProvider();
+
+            if (teacher.User == null)
+            {
+                throw new Exception(String.Format("Поля не заполнены!"));
+            }
+
+            if (userProvider.GetByID(teacher.ID) == null)
+            {
+                userProvider.Add(teacher.User);
+            }
+
+            if (GetByID(teacher.ID) == null)
+            {
+                teacherProvider.Add(teacher);
+
+                Role role = roleProvider.GetByName("Teacher");
+                userProvider.AddUserToRole(teacher.User, role);
+            }
+        }
     }
 }
