ebe3fa4e46bdf5c6c333d59de995d18ab95dfb3a
Daniil-Mel <Danya-Mel@mail.ru>
2015-02-06 15:18:48 +0300

    Правка провайдера RoleProvider

--- QuickDocs/Models/Domain/Providers/RoleProvider.cs
+++ QuickDocs/Models/Domain/Providers/RoleProvider.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NHibernate;
+using NHibernate.Criterion;
 using QuickDocs.Models.Domain.Entities;
 
 namespace QuickDocs.Models.Domain.Providers
 {
     public class RoleProvider:BaseProvider<Role>
     {
+        public Role GetByName(string name)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    var entity = session.CreateCriteria(typeof(Role))
+                        .Add(Expression.Eq("Name", name)).List<Role>().FirstOrDefault();
+                    return entity;
+                }
+            }
+        }
     }
 }
