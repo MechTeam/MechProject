6fdafd5f65a36e261aeb12d0161dee0aefd742fc
AndreyCruchok <cruchok.andrey@yandex.ru>
2015-02-15 23:07:53 +0300

    WQD-10 Разработать класс StudentControlWTO Унаследовать от класса ControlWTO. Конструктор на вход принимает три параметра типов: Control, ControlType, Discipline. Control с помощью base передать в конструктор класса родителя, а Discipline и Controle Type заполнить в конструкторе класса StudentControlWTO

---
+++ QuickDocs/Logic/WebTemplateObject/StudentControlWTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuickDocs.Models.Domain.Entities;
+
+namespace QuickDocs.Logic.WebTemplateObject
+{
+    public class StudentControlWTO:ControlWTO
+    {
+        string namecontrolType;
+        string namediscipline;
+
+        public StudentControlWTO(Control control,ControlType controlType,Discipline discipline):base(control)
+        {
+            controlType.Name=namecontrolType;
+            discipline.Name=namediscipline;
+        }
+
+        public string NameControlType
+        {
+            get { return namecontrolType; }
+        }
+        public string NameDiscipline
+        {
+            get { return namediscipline; }
+        }
+    }
+}
