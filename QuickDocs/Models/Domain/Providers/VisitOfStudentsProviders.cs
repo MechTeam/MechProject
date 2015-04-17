00417d1a09ed113b71717524ff8afe01fb598556
shurygin1996 <shurygin_1@mail.ru>
2015-04-17 19:58:59 +0300

    SQD-42 Добавить провайдера данных VisitsOfStudent

---
+++ QuickDocs/Models/Domain/Providers/VisitOfStudentsProviders.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickDocs.Models.Domain.Providers
+{
+    public class VisitOfStudentsProviders
+    {
+    }
+}
