8b0019ff1d28b54f1a7b6552f58b0e2d123eea3c
Daniil-Mel <Danya-Mel@mail.ru>
2015-04-17 19:22:15 +0300

    Создать класс для управления учебными группами

---
+++ QuickDocs/Models/Managers/StudentGroupManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuickDocs.Models.Domain.Entities;
+using QuickDocs.Models.Domain.Providers;
+
+namespace QuickDocs.Models.Managers
+{
+    public class StudentGroupManager
+    {
+        public void Transfer(StudentGroup LastGroup, StudentGroup NewGroup, Student student)
+        {
+            StudentGroupProvider studentGroupProvider = new StudentGroupProvider();
+            StudentProvider studentProvider = new StudentProvider();
+
+            for (int i = 0; i < LastGroup.Students.Count; i++)
+            {
+                if (LastGroup.Students[i] == student)
+                {
+                    studentGroupProvider.GetByID(LastGroup.ID).Students.RemoveAt(i);
+                    studentGroupProvider.GetByID(NewGroup.ID).Students.Add(student);
+                    student.StudentGroup = NewGroup;
+
+                    studentGroupProvider.Update(LastGroup);
+                    studentGroupProvider.Update(NewGroup);
+                    studentProvider.Update(student);
+                }
+            }
+        }
+
+        public void DeleteFromGroup(Student student)
+        {
+            StudentGroupProvider studentGroupProvider = new StudentGroupProvider();
+            StudentProvider studentProvider = new StudentProvider();
+
+            for (int i = 0; i < studentGroupProvider.GetByID(student.StudentGroup.ID).Students.Count; i++)
+            {
+                if (studentGroupProvider.GetByID(student.StudentGroup.ID).Students[i] == student)
+                {
+                    studentGroupProvider.GetByID(student.StudentGroup.ID).Students.RemoveAt(i);
+                    student.StudentGroup = null;
+
+                    studentGroupProvider.Update(studentGroupProvider.GetByID(student.StudentGroup.ID));
+                    studentProvider.Update(student);
+                }
+            }
+        }
+
+        public void AddToGroup(Student student, StudentGroup studentGroup)
+        {
+            StudentGroupProvider studentGroupProvider = new StudentGroupProvider();
+            StudentProvider studentProvider = new StudentProvider();
+
+            student.StudentGroup = studentGroup;
+            studentGroup.Students.Add(student);
+
+            studentGroupProvider.Update(studentGroup);
+            studentProvider.Update(student);
+        }
+
+        public void NameAutochanger(StudentGroup studentgroup)
+        {
+            DateTime time = new DateTime();
+            StudentGroupProvider studentGroupProvider = new StudentGroupProvider();
+
+            if (time.Day == 31 && (time.Month == 8 || time.Month == 1))
+            {
+                int number = Convert.ToInt32(studentgroup.Name[studentgroup.Name.Length - 2]);
+                int newNumber = number + 1;
+
+                studentgroup.Name.Insert(studentgroup.Name.Length - 2, Convert.ToString(newNumber));
+
+                studentGroupProvider.Update(studentgroup);
+            }
+        }
+    }
+}
